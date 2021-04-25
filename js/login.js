17f3ce1498c3dfc0e3fdaee95669d27183a93ca4
greshma <greshma.j1721@saintgits.org>
2021-04-25 13:22:22 +0530

    first commit

---
+++ js/login.js
@@ -0,0 +1,22 @@
+    let user=document.getElementById("username");
+    let pwd=document.getElementById("pwd");
+
+    function formSubmit(callback) {
+        $("form").on("submit", function(e){
+            e.preventDefault();
+            callback();
+        });
+    }
+    
+    formSubmit( function() {
+        if(user.value=="admin"&&pwd.value=="12345")
+        {
+        window.location = "./html/todo.html";
+        }
+        else{
+        alert("Please enter correct credentials")
+        }
+    });
+
+
+   
